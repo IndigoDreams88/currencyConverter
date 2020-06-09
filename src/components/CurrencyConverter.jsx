8fa2c02f8714aeca0e8fca0498803d1bf3ddb8cb
IndigoDreams88 <svidler2011@gmail.com>
2020-06-09 18:20:19 +0100

    added API link and Github repo link

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -57,78 +57,96 @@ class CurrencyConverter extends Component {
     } = this.state;
 
     return (
-      <div className={styles.converter}>
-        <h1 className={styles.title}>Currency Converter</h1>
-        <div className={styles.currencyList}>
-          <form className={styles.form} onSubmit={this.handleConversion}>
-            <label className={styles.label}>Amount:</label>
+      <div className={styles.outerContainer}>
+        <center>
+          <div className={styles.converter}>
+            <h1 className={styles.title}>Currency Converter</h1>
+            <div className={styles.currencyList}>
+              <form className={styles.form} onSubmit={this.handleConversion}>
+                <label className={styles.label}>Amount:</label>
 
-            <input
-              type="number"
-              name="amount"
-              value={amount}
-              className={styles.inputBox}
-              onChange={(event) =>
-                this.setState({ amount: event.target.value })
-              }
-            />
+                <input
+                  type="number"
+                  name="amount"
+                  value={amount}
+                  className={styles.inputBox}
+                  onChange={(event) =>
+                    this.setState({ amount: event.target.value })
+                  }
+                />
 
-            <select
-              className={styles.selectFrom}
-              name="from"
-              onChange={(event) => this.selectCurrency(event)}
-              value={fromCurrency}
-            >
-              {currencyNames.map((currencyName) => {
-                return (
-                  <option
-                    key={currencyName}
-                    value={currencyName}
-                    className={styles.fromCurrencies}
-                  >
-                    {currencyName}
-                  </option>
-                );
-              })}
-            </select>
-            <label className={styles.label2}>convert to:</label>
+                <select
+                  className={styles.selectFrom}
+                  name="from"
+                  onChange={(event) => this.selectCurrency(event)}
+                  value={fromCurrency}
+                >
+                  {currencyNames.map((currencyName) => {
+                    return (
+                      <option
+                        key={currencyName}
+                        value={currencyName}
+                        className={styles.fromCurrencies}
+                      >
+                        {currencyName}
+                      </option>
+                    );
+                  })}
+                </select>
+                <label className={styles.label2}>convert to:</label>
 
-            <select
-              className={styles.selectTo}
-              name="to"
-              onChange={(event) => this.selectCurrency(event)}
-              value={toCurrency}
-            >
-              {currencyNames.map((currencyName) => {
-                return (
-                  <option
-                    key={currencyName}
-                    value={currencyName}
-                    className={styles.toCurrencies}
-                  >
-                    {currencyName}
-                  </option>
-                );
-              })}
-            </select>
-            <button type="submit" className={styles.convertButton}>
-              Convert
-            </button>
-            <center>
-              <div>
-                <p className={styles.exchangeRateText}>
-                  Exchange Rate ={" "}
-                  {this.state.Data
-                    ? this.state.Data.rates[this.state.toCurrency]
-                    : null}
-                </p>
-              </div>
-              <div className={styles.convertedAmountDisplay}>
-                {convertedAmount}
-              </div>
-            </center>
-          </form>
-        </div>
+                <select
+                  className={styles.selectTo}
+                  name="to"
+                  onChange={(event) => this.selectCurrency(event)}
+                  value={toCurrency}
+                >
+                  {currencyNames.map((currencyName) => {
+                    return (
+                      <option
+                        key={currencyName}
+                        value={currencyName}
+                        className={styles.toCurrencies}
+                      >
+                        {currencyName}
+                      </option>
+                    );
+                  })}
+                </select>
+                <button type="submit" className={styles.convertButton}>
+                  Convert
+                </button>
+                <center>
+                  <div>
+                    <p className={styles.exchangeRateText}>
+                      Exchange Rate ={" "}
+                      {this.state.Data
+                        ? this.state.Data.rates[this.state.toCurrency]
+                        : null}
+                    </p>
+                  </div>
+                  <div className={styles.convertedAmountDisplay}>
+                    {convertedAmount}
+                  </div>
+                </center>
+              </form>
+            </div>
+          </div>
+        </center>
+        <>
+          <center>
+            <p>
+              Built using this{" "}
+              <a href="https://api.exchangerate-api.com/v4/latest/USD">API</a>
+            </p>
+            <p>
+              View the code:{" "}
+              <a href="https://github.com/IndigoDreams88/currencyConverter">
+                Github
+              </a>
+            </p>
+          </center>
+        </>
       </div>
     );
   }
